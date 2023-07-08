d8ef73e25d18e92c1b22f82f748a0fbda849ec66
leilafaez <leila.faez@gmail.com>
2023-07-08 14:25:45 +0100

    install and apply scss(sass) for styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/main.css"
+import "../styles/main.scss"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
