befdd053f1ab78d92e4874311112932c83315ec8
leilafaez <leila.faez@gmail.com>
2023-07-10 18:47:10 +0100

    implement the api for getting data from server

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -2,9 +2,19 @@ import BaseLayout from "@/component/layouts/BaseLayout";
 import axios from "axios";
 import Link from "next/link";
 import BasePage from "@/component/BasePage";
+import { useEffect } from "react";
 
 
 const Portfolio = ({ posts }) => {
+
+  useEffect(()=>{
+    async function getPosts(){
+      const res = await fetch(`/api/v1/posts`);
+      const data = await res.json();
+    }
+    getPosts();
+  },[])
+
   const renderPosts = () => {
     return posts.map((post) =>
      <li key={post.id}>
