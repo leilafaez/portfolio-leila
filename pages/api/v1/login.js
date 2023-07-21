a0932c22a7e575125ce8cc9ddace5920200b57fc
leilafaez <leila.faez@gmail.com>
2023-07-21 14:55:27 +0100

    fix some issue

--- pages/api/v1/login.js
+++ pages/api/v1/login.js
@@ -1,5 +1,12 @@
+import auth0 from "@/utils/auth0"
 
+export default async function login(req,res){
 
-export default function login(req,res){
-    res.status(200).json({message : "hello login!"})
+    try{
+        await auth0.handleLogin(req,res);
+
+    }catch(error){
+        console.log(error);
+        res.status(error.status || 400).end(error.message);
+    }
 }
