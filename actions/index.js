a806a1c5d91ffc437085e3e6d7d4562b782dedbf
leilafaez <leila.faez@gmail.com>
2023-07-24 17:06:11 +0100

    get user info  by implementing handleProfile from auth0

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then(async res=> {
+export const fetcher = (url) => fetch(url).then(async res=> {
     const result =await res.json();
     if (res.status !== 200) {
         return Promise.reject(result);
