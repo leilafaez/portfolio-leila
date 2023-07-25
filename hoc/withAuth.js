9ebec1f378ea3e9cb45e4116ad230747c37600cd
leilafaez <leila.faez@gmail.com>
2023-07-25 22:49:01 +0100

    implement withAuth HOC

--- hoc/withAuth.js
+++ hoc/withAuth.js
@@ -1,10 +1,23 @@
+import { useGetUser } from "@/actions/user";
+import Redirect from "@/component/shared/Redirect";
 
+const withAuth = (Component) => {
+  return (props) => {
+    const { data, loading } = useGetUser();
 
-
-const withAuth =(Component)=>{
-    return props => {
-      return  <Component {...props}/>
+    if (loading) {
+      return <p>Loading...</p>;
     }
-}
+
+     if (!data) {
+       return <Redirect to="/api/v1/login" />;
+     } else {
+       return <Component data={data} loading={loading} {...props} />;    
+       
+     }
+  };
+
+ 
+};
 
 export default withAuth;
--- pages/secret.js
+++ pages/secret.js
@@ -1,42 +1,22 @@
 import BaseLayout from "@/component/layouts/BaseLayout";
 import BasePage from "@/component/BasePage";
-import { useGetUser } from "@/actions/user";
-import Redirect from "@/component/shared/Redirect";
 import withAuth from "@/hoc/withAuth";
 
-const Secret = () => {
-  const { data, loading } = useGetUser();
 
-  if(loading){
-    return<p>Loading...</p>
-  }
-
-  if(!data){
-    return <Redirect to="/api/v1/login"/>
-  }else{
-    return (
-      <BaseLayout user={data} loading={loading}>
-        <BasePage>
-          <h1>this Secret page </h1>
-        </BasePage>
-      </BaseLayout>
-    );
-
-  }
+const Secret = ({data,loading}) => {
+  
+return(
+  <BaseLayout data={data} loading={loading}>
+    <BasePage>
+      <h1>This is a secret page - Hello{data.name} </h1>
+    </BasePage>
+  </BaseLayout>
+)
+  
 
 
 };
 
-//High Order Component - HOC
-//simple function that takes a component and returns 
-//new component with some extended functionality
 
-// function withAuth(Component) {
-//   return function(props){
-//     return <Component title="Hello World" {...props}/>
-//   }
-// }
-
-// export default withAuth(Secret)
 
 export default withAuth(Secret);
