7bee66731ef6faf6ecae36337141a1f1126e6145
leilafaez <leila.faez@gmail.com>
2023-07-25 22:23:22 +0100

    Use HOC for secret page

--- pages/secret.js
+++ pages/secret.js
@@ -2,6 +2,7 @@ import BaseLayout from "@/component/layouts/BaseLayout";
 import BasePage from "@/component/BasePage";
 import { useGetUser } from "@/actions/user";
 import Redirect from "@/component/shared/Redirect";
+import withAuth from "@/hoc/withAuth";
 
 const Secret = () => {
   const { data, loading } = useGetUser();
@@ -16,7 +17,7 @@ const Secret = () => {
     return (
       <BaseLayout user={data} loading={loading}>
         <BasePage>
-          <h1>this Secret page</h1>
+          <h1>this Secret page </h1>
         </BasePage>
       </BaseLayout>
     );
@@ -26,4 +27,16 @@ const Secret = () => {
 
 };
 
-export default Secret;
+//High Order Component - HOC
+//simple function that takes a component and returns 
+//new component with some extended functionality
+
+// function withAuth(Component) {
+//   return function(props){
+//     return <Component title="Hello World" {...props}/>
+//   }
+// }
+
+// export default withAuth(Secret)
+
+export default withAuth(Secret);
