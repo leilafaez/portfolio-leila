996c4a1533e6175ebd1257e5726f8922df5567ff
leilafaez <leila.faez@gmail.com>
2023-07-26 20:03:41 +0100

    get user session from server side and implement server side function

---
+++ pages/secretssr.js
@@ -0,0 +1,29 @@
+import BaseLayout from "@/component/layouts/BaseLayout";
+import BasePage from "@/component/BasePage";
+import auth0 from "@/utils/auth0";
+
+
+const SecretSSR = ({ user }) => {
+  return (
+    <BaseLayout user={user} loading={false}>
+      <BasePage>
+        <h1>I am Secret Page - Hello {user && user.name}</h1>
+      </BasePage>
+    </BaseLayout>
+  );
+};
+export const getServerSideProps = async ({ req, res }) => {
+  const session = await auth0.getSession(req);
+  if (!session || !session.user) {
+    res.writeHead(302, {
+      Location: "/api/v1/login",
+    });
+    res.end();
+    return { props: {} };
+  }
+  return {
+    props: { user: session.user },
+  };
+};
+
+export default SecretSSR;
