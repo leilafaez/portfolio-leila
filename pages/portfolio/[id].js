a6e119419da1f681cc65ec2becb6394261ed9988
leilafaez <leila.faez@gmail.com>
2023-08-11 11:49:49 +0100

    get static paths for portfolios and move the url to the env file

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -21,11 +21,31 @@ const Portfolio = ({portfolio})=>{
     );
 }
 
-export async function getServerSideProps({query}){
-  const json= await new PortfolioApi().getById(query.id);
-  const portfolio=json.data;
+// export async function getServerSideProps({query}){
+//   const json= await new PortfolioApi().getById(query.id);
+//   const portfolio=json.data;
 
-  return{props : {portfolio}};
+//   return{props : {portfolio}};
+// }
+
+//This function is executed at the build time
+export async function getStaticPaths(){
+  const json = await new PortfolioApi().getAll();
+  const portfolio =json.data;
+//Get the paths we want to pre-render based on portfolio ID
+  const paths = portfolio.map(port=>{
+    return{
+      params : {id : port._id}
+    }
+  })
+  //fallback:false means that "not found pages" will be resolved into 404 page
+  return {paths,fallback:false}
+}
+
+export async function getStaticProps({params}) {
+  const json =await new PortfolioApi().getById(params.id);
+  const portfolio = json.data;
+  return {props :{portfolio}}
 }
 
 export default Portfolio;
