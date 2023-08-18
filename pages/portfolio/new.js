ce85a0cade1780149f45f48496fbeadafd18d16c
leilafaez <leila.faez@gmail.com>
2023-08-18 13:10:10 +0100

    create a form for adding portfolio

--- pages/portfolio/new.js
+++ pages/portfolio/new.js
@@ -1,11 +1,18 @@
 import BaseLayout from "@/component/layouts/BaseLayout";
 import BasePage from "@/component/BasePage";
 import withAuth from "@/hoc/withAuth";
+import {Row,Col} from "reactstrap";
+import PortfolioForm from "@/component/PortfolioForm";
 
 const PortfolioNew = ({user,loading}) => {
   return (
     <BaseLayout user={user} loading={loading}>
       <BasePage header="Create Portfolio">
+        <Row>
+          <Col md="8">
+           <PortfolioForm/>
+          </Col>
+        </Row>
       </BasePage>
     </BaseLayout>
   );
