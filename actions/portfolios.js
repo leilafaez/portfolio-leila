f2d00b91ef1d72be00dc3b161e0e0e2f9f4049b4
leilafaez <leila.faez@gmail.com>
2023-09-19 16:10:11 +0100

    complete PATCH request on client

--- actions/portfolios.js
+++ actions/portfolios.js
@@ -4,7 +4,11 @@ import useSWR from "swr";
 import { fetcher } from "./index";
 
 const createPortfolio = (data) => axios.post("/api/v1/portfolios", data);
+const updatePortfolio = (id,data) => axios.patch(`/api/v1/portfolios/${id}`, data);
+
 export const useCreatePortfolio = () => useApiHandler(createPortfolio);
+export const useUpdatePortfolio = () => useApiHandler(updatePortfolio);
+
 
 export const useGetPortfolio = (id) => {
   const { data, error, ...rest } = useSWR(id ? `/api/v1/portfolios/${id}` :null , fetcher);
--- pages/portfolio/[id]/edit.js
+++ pages/portfolio/[id]/edit.js
@@ -2,23 +2,31 @@ import BaseLayout from "@/component/layouts/BaseLayout";
 import BasePage from "@/component/BasePage";
 import withAuth from "@/hoc/withAuth";
 import { useRouter } from "next/router";
-import { useGetPortfolio } from "@/actions/portfolios";
+import { useGetPortfolio, useUpdatePortfolio } from "@/actions/portfolios";
 import PortfolioForm from "@/component/PortfolioForm";
 import { Col, Row } from "reactstrap";
 
 
 const PortfolioEdit = ({user}) => {
   const router =useRouter();
-  const {data}=useGetPortfolio(router.query.id);
-  debugger
+  const [updatePortfolio, { data, error, loading }] = useUpdatePortfolio();
+  const {data : initialData}=useGetPortfolio(router.query.id);
+  
+  const _updatePortfolio =()=>{
+    updatePortfolio(router.query.id,data);
+  }
+
   return (
     <BaseLayout user={user} loading={false}>
       <BasePage header="Portfolio Edit">
         <Row>
           <Col md="8">
-            {data &&
-              <PortfolioForm onSubmit={(data)=>alert(JSON.stringify(data))} initialData={data} />
-            }
+            {initialData && (
+              <PortfolioForm
+                onSubmit={_updatePortfolio}
+                initialData={initialData}
+              />
+            )}
           </Col>
         </Row>
       </BasePage>
