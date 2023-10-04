dd3012300b523ccdee385158fb38e079da6c69a7
leilafaez <leila.faez@gmail.com>
2023-10-04 17:13:33 +0100

    design portfolio detail page

--- component/BasePage.js
+++ component/BasePage.js
@@ -1,21 +1,26 @@
 import { Container } from "reactstrap";
-
+import head from "next/head";
 
 
 const BasePage = props=>{
     const{className="",header,children} = props;
-    return(
+    return (
+      <>
+      <head>
+        <tit>Leila Faez Portfolio</tit>
+      </head>
         <div className={`base-page ${className}`}>
-            <Container>
-                {header &&
-                <div className="page-header">
-                    <h1 className="page-header-title">{header}</h1>
-                </div>
-                }
+          <Container>
+            {header && (
+              <div className="page-header">
+                <h1 className="page-header-title">{header}</h1>
+              </div>
+            )}
             {children}
-            </Container>
+          </Container>
         </div>
-    )
+      </>
+    );
 }
 
 export default BasePage;
--- pages/portfolio/[id]/index.js
+++ pages/portfolio/[id]/index.js
@@ -9,8 +9,25 @@ const Portfolio = ({ portfolio }) => {
   const { data: dataU, loading: loadingU } = useGetUser();
 
   return (
-    <BaseLayout user={dataU} loading={loadingU}>
-      <BasePage header="Portfolio Detail">{JSON.stringify(portfolio)}</BasePage>
+    <BaseLayout user={dataU} loading={loadingU} navClass="transparent">
+      <BasePage className="indexPage noWrapper">
+        <div className="portfolio-detail">
+          <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            <main role="main" class="inner page-cover">
+              <h1 class="cover-heading">Title</h1>
+              <p class="lead dates">dates</p>
+              <p class="lead info mb-0">jobTitle | company | location</p>
+              <p class="lead">description</p>
+              <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary">
+                  Visit Company
+                </a>
+              </p>
+            </main>
+          </div>
+        </div>
+        ;
+      </BasePage>
     </BaseLayout>
   );
 };
