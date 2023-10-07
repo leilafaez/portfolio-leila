eac039aa7ae5fddfc068bf782498cd4fa859e597
leilafaez <leila.faez@gmail.com>
2023-10-07 11:47:24 +0100

    portfolio detail data

--- pages/portfolio/[id]/index.js
+++ pages/portfolio/[id]/index.js
@@ -2,6 +2,7 @@ import BaseLayout from "@/component/layouts/BaseLayout";
 import BasePage from "@/component/BasePage";
 import { useRouter } from "next/router";
 import { useGetUser } from "@/actions/user";
+import {formatDate} from "@/helpers/function"
 import PortfolioApi from "@/lib/api/portfolios";
 
 const Portfolio = ({ portfolio }) => {
@@ -9,17 +10,26 @@ const Portfolio = ({ portfolio }) => {
   const { data: dataU, loading: loadingU } = useGetUser();
 
   return (
-    <BaseLayout user={dataU} loading={loadingU} >
-      <BasePage className="indexPage noWrapper">
+    <BaseLayout user={dataU} loading={loadingU}>
+      <BasePage className="detail-page">
         <div className="portfolio-detail">
           <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
             <main role="main" class="inner page-cover">
               <h1 class="cover-heading">{portfolio.title}</h1>
-              <p class="lead dates">{portfolio.startDate}-{portfolio.endDate}</p>
-              <p class="lead info mb-0">{portfolio.jobTitle} | {portfolio.company} | {portfolio.location}</p>
+              <p class="lead dates">
+                {formatDate(portfolio.startDate)}--{formatDate(portfolio.endDate) || "Present"}
+              </p>
+              <p class="lead info mb-0">
+                {portfolio.jobTitle} | {portfolio.company} |{" "}
+                {portfolio.location}
+              </p>
               <p class="lead">{portfolio.description}</p>
               <p class="lead">
-                <a href={portfolio.companyWebsite} target="_" class="btn btn-lg btn-secondary">
+                <a
+                  href={portfolio.companyWebsite}
+                  target="_"
+                  class="btn btn-lg btn-secondary"
+                >
                   Visit Company
                 </a>
               </p>
